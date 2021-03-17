b0b1cb85d2852a6df05b8ce3b6b077ed7fcbf6b5
Shivalingesh1999 <shivalingesh777@gmail.com>
2021-03-17 10:56:15 +0530

    completed

---
+++ ex.java
@@ -0,0 +1,31 @@
+class P{
+
+public static void main(String args[])
+{
+      System.out.println("inside p");
+	  main(10);
+}
+public static void main(String args[])
+{
+      System.out.println("inside Q");
+}
+}
+class Q
+{
+  public static void main(String args[])
+    {
+      System.out.println("inside Q");
+    }
+
+
+}
+
+
+class R
+  {
+   public static void main(String args[])
+    {
+      System.out.println("inside R");
+    }
+	
+  }
